ab309282a7a68db0887a50048c8f2d193eb634b8
Ana Zubieta Pena <azubieta@c2r8s2.42malaga.com>
2025-08-07 15:26:38 +0200

    modificando norminette de ex01

--- M_01/ex01/src/main.cpp
+++ M_01/ex01/src/main.cpp
@@ -6,14 +6,12 @@ int main(void)
         int i = 0;
         int n = 10;
         Zombie* horde = zombieHorde(n, "Zombie");
-        std::cout << "------------------------------" << std::endl;
         while (i < n)
         {
+            std::cout << "------------------------------" << std::endl;
             horde[i].announce();
             i++;
         }
-        std::cout << "------------------------------" << std::endl;
         deleteHorde(n, horde);
-        std::cout << "------------------------------" << std::endl;
     }
 }
--- M_01/ex01/src/zombieHorde.cpp
+++ M_01/ex01/src/zombieHorde.cpp
@@ -2,7 +2,7 @@
 
 static std::string  to_string(const int n)
 {
-    std::stringstream str;
+    std::stringstream   str;
     str << n;
     return (str.str());
 }
@@ -12,11 +12,12 @@ Zombie* zombieHorde(const int N, const std::string nameValue)
     if (N <= 0)
         return (NULL);
 
-    Zombie*  zombieHorde = new Zombie[N];
+    Zombie* zombieHorde = new Zombie[N];
 
     int i = 0;
     while (i < N)
     {
+        std::cout << "------------------------------" << std::endl;
         zombieHorde[i].setName(nameValue + to_string(0) + to_string(i));
         i++;
     }
@@ -28,8 +29,10 @@ void    deleteHorde(const int N, Zombie* zombieHorde)
     int i = 0;
     while (i < N)
     {
+        std::cout << "------------------------------" << std::endl;
         zombieHorde[i].getName();
         i++;
     }
+    std::cout << "------------------------------" << std::endl;
     delete[] zombieHorde;
 }
