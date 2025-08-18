8e46e5718acb17fbe6f3ab7b2385ddbd3fae0f92
Ana Zubieta Pena <azubieta@c1r12s2.42malaga.com>
2025-08-18 16:53:31 +0200

    modicifacion informacion debugueo

--- M_02/ex01/src/main.cpp
+++ M_02/ex01/src/main.cpp
@@ -1,8 +1,11 @@
 #include "Fixed.hpp"
 
-
 int main( void )
 {
+    std::cout << a << std::endl;
+    std::cout << "\na is " << a.getRawBits() << " as fixed-point" << std::endl;
+    std::cout << "\na is " << a.toFloat() << " as float" << std::endl;
+    std::cout << "\na is " << a.toInt() << " as integer" << std::endl;
     std::cout << "-----------------------A------------------" << std::endl;
     Fixed a;
     a = Fixed(1234.4321f);
@@ -17,22 +20,22 @@ int main( void )
     Fixed const d(b);
 
     std::cout << "-----------------------FIXED-POINT------------------" << std::endl;
-    std::cout << "a is " << a.getRawBits() << " as fixed-point" << std::endl;
-    std::cout << "b is " << b.getRawBits() << " as fixed-point" << std::endl;
-    std::cout << "c is " << c.getRawBits() << " as fixed-point" << std::endl;
-    std::cout << "d is " << d.getRawBits() << " as fixed-point" << std::endl;
+    std::cout << "\na is " << a.getRawBits() << " as fixed-point" << std::endl;
+    std::cout << "\nb is " << b.getRawBits() << " as fixed-point" << std::endl;
+    std::cout << "\nc is " << c.getRawBits() << " as fixed-point" << std::endl;
+    std::cout << "\nd is " << d.getRawBits() << " as fixed-point" << std::endl;
     
     std::cout << "-----------------------FLOAT------------------" << std::endl;
-    std::cout << "a is " << a << " as float" << std::endl;
-    std::cout << "b is " << b << " as float" << std::endl;
-    std::cout << "c is " << c << " as float" << std::endl;
-    std::cout << "d is " << d << " as float" << std::endl;
+    std::cout << "\na is " << a.toFloat() << " as float" << std::endl;
+    std::cout << "\nb is " << b.toFloat() << " as float" << std::endl;
+    std::cout << "\nc is " << c.toFloat() << " as float" << std::endl;
+    std::cout << "\nd is " << d.toFloat() << " as float" << std::endl;
 
     std::cout << "-----------------------INT------------------" << std::endl;
-    std::cout << "a is " << a.toInt() << " as integer" << std::endl;
-    std::cout << "b is " << b.toInt() << " as integer" << std::endl;
-    std::cout << "c is " << c.toInt() << " as integer" << std::endl;
-    std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+    std::cout << "\na is " << a.toInt() << " as integer" << std::endl;
+    std::cout << "\nb is " << b.toInt() << " as integer" << std::endl;
+    std::cout << "\nc is " << c.toInt() << " as integer" << std::endl;
+    std::cout << "\nd is " << d.toInt() << " as integer" << std::endl;
 
     std::cout << "-----------------------------------------" << std::endl;
 
--- M_02/ex01/src/utils.cpp
+++ M_02/ex01/src/utils.cpp
@@ -2,6 +2,10 @@
 
 std::ostream    &operator<<(std::ostream& fd, const Fixed& object)
 {
+    std::cout
+        << "\nExtern: Operator overload"
+        << std::endl;
+    
     fd << object.toFloat();
     
     return (fd);
